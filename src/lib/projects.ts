4f91027c61a326fb513af4d44fedc43a8d5ed091
Hanif Carroll <hanif@hanifcarroll.com>
2026-04-29 22:19:19 -0300

    Add Apartment Finder case study

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -9,10 +9,12 @@ import casaElariaData from './projects/casa-elaria.json';
 import desarmaderoLatorreData from './projects/desarmadero-latorre.json';
 import maximoInteriorismoData from './projects/maximo-interiorismo.json';
 import redwriterComicsData from './projects/redwriter-comics.json';
+import apartmentFinderData from './projects/apartment-finder.json';
 
 export const projects: Record<string, Project> = {
   'palabruno': palabrunoData as Project,
   'ba-eventos': baEventosData as Project,
+  'apartment-finder': apartmentFinderData as Project,
   'genrupt': genruptData as Project,
   'mucho-hangouts': muchoHangoutsData as Project,
   'language-exchange': languageExchangeData as Project,
